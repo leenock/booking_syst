4eb6a5bbec20a533b4dafde14dd039f261364e30
leenock <leenock176@gmail.com>
2025-03-19 15:33:09 +0300

    .

---
+++ app/(admin)/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Admin Dashboard | Vicarage Resorts',
+  description: 'Admin dashboard for managing Vicarage Resorts',
+}; 
