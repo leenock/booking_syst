0742335bf3a3f0a4a4d3c4f23501aba4ad313984
leenock <leenock176@gmail.com>
2025-03-19 15:38:40 +0300

    delted files

--- app/(admin)/metadata.ts
+++
@@ -1,6 +0,0 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Admin Dashboard | Vicarage Resorts',
-  description: 'Admin dashboard for managing Vicarage Resorts',
-}; 
